156ac9948e2dca5cbfd41943bb7daa9a1d0b2f0b
priyanshisaraf <priyanshisaraf9@gmail.com>
2025-06-10 08:10:34 +0530

    Updated App.jsx

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -72,7 +72,7 @@ function App() {
             !user ? (
               <PageWrapper><SignUp /></PageWrapper>
             ) : (
-              <Navigate to="/" />
+              <Navigate to="/login" />
             )
           }
         />
